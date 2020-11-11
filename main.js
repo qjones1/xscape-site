bcce5fd6294c9e043459b3702513d3c57ec0a4f5
Brittini <bbarnes@pivottechschool.com>
2020-11-10 19:16:34 -0500

    Updates to Merch page

--- main.js
+++ main.js
@@ -2,62 +2,62 @@ const merchItems = [
    
     {
         type: 'clothes',
-        title: 'T-shirt',
-        price: 36.00,
+        title: 'Xscape T-shirt',
+        price: '$36',
         imgUrl: 'https://i.imgur.com/iMbqr0u.jpg',
     },
     {
         type: 'hat',
-        title: 'Beanie', 
-        price: 26.00,
+        title: 'Xscape Beanie', 
+        price: '$26',
         imgUrl: 'https://i.imgur.com/nqTTDJ3.jpg',
     },
     {
         type: 'hat',
-        title: 'Trucker',
-        price: 30.00,
+        title: 'Xscape Trucker Hat',
+        price: '$30',
         imgUrl: 'https://i.imgur.com/TiPGP1J.jpeg',
     },
     {
         type: 'bag',
-        title: 'Pouch',
-        price: 16.00,
+        title: 'Xscape Pouch',
+        price: '$16',
         imgUrl: 'https://i.imgur.com/gkQlgGc.jpg',
     },
     {
         type: 'bag',
-        title: 'Tote Bag',
-        price: 20.00,
+        title: 'Xscape Tote Bag',
+        price: '$20',
         imgUrl: 'https://i.imgur.com/rd9ZlTW.jpeg',
     },
     {
         type: 'home',
-        title: 'Xscape Mug',
-        price: 12.00,
+        title: 'Xscape Coffee Mug',
+        price: '$12',
         imgUrl: 'https://i.imgur.com/lVzDmsC.jpg',
     },
     {
         type: 'home',
-        title: 'Xscape Pillow',
-        price: 30.00,
+        title: 'Xscape Throw Pillow',
+        price: '$30',
         imgUrl: 'https://i.imgur.com/R97JhCW.jpeg',
     },
     {
         type: 'clothes',
         title: 'Xscape Hoodie',
-        price: 40.00,
+        price: '$40',
         imgUrl: 'https://i.imgur.com/PAj6Xlm.jpg',
     },
     {
         type: 'accessory',
         title: 'Xscape Phone Case',
-        price: 26.00,
+        price: '$26',
         imgUrl: 'https://i.imgur.com/6pMfcc1.jpeg',
     },
     {
         type: 'accessory',
         title: 'Xscape Mask',
-        price: 16.00,
+        price: '$16',
         imgUrl: 'https://i.imgur.com/8dpIe9B.jpeg',
     },
 ]
@@ -69,20 +69,20 @@ const printToDom = (divId, textToPrint) => {
 
 const merchPrinter = (merchItems) => {
     let domString = '';
-    for (i=0; i<merchItems.length; i++) {
+    for (i=0; i < merchItems.length; i++) {
         domString += `
             <div class="card" style="width: 18rem;">
                 <img src="${merchItems[i].imgUrl}" class="card-img-top" alt="...">
                     <div class="card-body">
                      <h5 class="card-title">${merchItems[i].title}</h5>
                         <h5 class="card-price">${merchItems[i].price}</h5>
-                      <a href="#" class="btn btn-dark">Add to Cart</a>
+                        <button onclick="purchaseAlert()" id="btn-5" class="btn btn-dark purchase-btn-group button--5">Add to Cart</button>
                     </div>
             </div>
         `
     }
     console.log(merchPrinter)
-    printToDom('merchItems', domString)
+    printToDom('merchCards', domString)
 }
 
   const init = () => { 
@@ -90,3 +90,107 @@ const merchPrinter = (merchItems) => {
   }
   init()
 
+const xClothes = merchItems.filter(item => item.type === 'clothes')
+const xHat = merchItems.filter(item => item.type === 'hat')
+const xBag = merchItems.filter(item => item.type === 'bag')
+const xHome = merchItems.filter(item => item.type === 'home')
+const xAccessory = merchItems.filter(item => item.type === 'accessory')
+const all = merchItems.filter(item => item.type === 'clothes' || 'hat' || 'bag' || 'home' || 'accessory')
+
+
+const printClothes = () => {
+    document.getElementById("merchCards").innerHTML = merchItems.innerHTML = xClothes.map(function (clothes) {
+        return `
+        <div class="card" style="width: 18rem;">
+                <img src="${clothes.imgUrl}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                     <h5 class="card-title">${clothes.title}</h5>
+                        <h5 class="card-price">${clothes.price}</h5>
+                        <button onclick="purchaseAlert()" id="btn-5" class="btn btn-dark purchase-btn-group button--5">Add to Cart</button>                    </div>
+            </div>
+        `; 
+    }).join('');
+}
+
+const printHat = () => {
+    document.getElementById("merchCards").innerHTML = merchItems.innerHTML = xHat.map(function (hat) {
+        return `
+        <div class="card" style="width: 18rem;">
+                <img src="${hat.imgUrl}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                     <h5 class="card-title">${hat.title}</h5>
+                        <h5 class="card-price">${hat.price}</h5>
+                        <button onclick="purchaseAlert()" id="btn-5" class="btn btn-dark purchase-btn-group button--5">Add to Cart</button>                    </div>
+            </div>
+        `;
+    }).join('');
+}
+
+const printBag = () => {
+    document.getElementById("merchCards").innerHTML = merchItems.innerHTML = xBag.map(function (bag) {
+        return `
+        <div class="card" style="width: 18rem;">
+                <img src="${bag.imgUrl}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                     <h5 class="card-title">${bag.title}</h5>
+                        <h5 class="card-price">${bag.price}</h5>
+                        <button onclick="purchaseAlert()" id="btn-5" class="btn btn-dark purchase-btn-group button--5">Add to Cart</button>                    </div>
+            </div>
+        `;
+    }).join('');
+}
+
+const printHome = () => {
+    document.getElementById("merchCards").innerHTML = merchItems.innerHTML = xHome.map(function (home) {
+        return `
+        <div class="card" style="width: 18rem;">
+                <img src="${home.imgUrl}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                     <h5 class="card-title">${home.title}</h5>
+                        <h5 class="card-price">${home.price}</h5>
+                        <button onclick="purchaseAlert()" id="btn-5" class="btn btn-dark purchase-btn-group button--5">Add to Cart</button>                    </div>
+            </div>
+        `;
+    }).join('');
+}
+
+const printAssessories = () => {
+    document.getElementById("merchCards").innerHTML = merchItems.innerHTML = xAccessory.map(function (accessory) {
+        return `
+        <div class="card" style="width: 18rem;">
+                <img src="${accessory.imgUrl}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                     <h5 class="card-title">${accessory.title}</h5>
+                        <h5 class="card-price">${accessory.price}</h5>
+                        <button onclick="purchaseAlert()" id="btn-5" class="btn btn-dark purchase-btn-group button--5">Add to Cart</button>                    </div>
+            </div>
+        `;
+    }).join('');
+}
+
+const printAll = () => {
+    document.getElementById("merchCards").innerHTML = merchItems.innerHTML = all.map(function (all) {
+        return `
+        <div class="card" style="width: 18rem;">
+                <img src="${all.imgUrl}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                     <h5 class="card-title">${all.title}</h5>
+                        <h5 class="card-price">${all.price}</h5>
+                        <button onclick="purchaseAlert()" id="btn-5" class="btn btn-dark purchase-btn-group button--5">Add to Cart</button>                    </div>
+            </div>
+        `;
+    }).join('');
+}
+const purchaseAlert = () => {
+    alert("Added to Cart")
+}
+
+document.getElementById('clothes-btn').addEventListener('click', printClothes);
+document.getElementById('hat-btn').addEventListener('click', printHat);
+document.getElementById('bag-btn').addEventListener('click', printBag);
+document.getElementById('home-btn').addEventListener('click', printHome)
+document.getElementById('accessories-btn').addEventListener('click', printAssessories)
+document.getElementById('all-btn').addEventListener('click', printAll)
+
+
+console.log(xClothes)
